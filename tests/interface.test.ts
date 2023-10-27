bd2f2855718ea4c35e6c257e0a0bf2c27ed99d70
João Teixeira <jpedroteix99@gmail.com>
2023-10-27 01:44:58 +0100

    add unit tests.

--- tests/interface.test.ts
+++ tests/interface.test.ts
@@ -1,38 +1,31 @@
+import axios from "axios";
 import Interface from "../src/interface.js";
 
-class DummyInterface extends Interface {
-  constructor(baseURLs?: Interface["_baseURLs"]) {
-    super();
-    this._baseURLs = baseURLs;
-  }
+jest.mock("axios");
 
+class DummyInterface extends Interface {
   public async get() {
-    return this._get("/getCompletedEvents");
+    return this._get(this._baseURLs.main, "/getCompletedEvents", {
+      query: {
+        hl: "en-US",
+        tournamentId: ["110852926142971547"],
+      },
+    });
   }
 }
 
 describe("api interface implementation", () => {
-  jest.mock("axios");
+  const mockedAxios = jest.mocked(axios);
+  const mockedAPIResponseData = {};
 
-  it(`"get" method should throw when it's called while the instance's "_baseURLs" property is undefined`, async () => {
-    const dummyInterface = new DummyInterface();
-    await expect(dummyInterface.get()).rejects.toThrow();
+  mockedAxios.get.mockResolvedValue({
+    status: 200,
+    data: mockedAPIResponseData,
   });
 
-  it(`"get" method should throw when both "_baseURLs" are unreachable`, async () => {
-    const dummyInterface = new DummyInterface(["a", "b"]);
-    await expect(dummyInterface.get()).rejects.toThrow();
+  const dummyInterface = new DummyInterface();
+
+  it(`"get" method should return a value when the request was successful`, async () => {
+    await expect(dummyInterface.get()).resolves.toBe(mockedAPIResponseData);
   });
-
-  // it.todo(`should return a value when the request was successful`, async () => {
-  //   const expectedData = {};
-
-  //   mockedAxios.get.mockResolvedValue({
-  //     status: 200,
-  //     data: expectedData,
-  //   });
-
-  //   const dummyInterface = new DummyInterface(["a"]);
-  //   await expect(dummyInterface.get()).resolves.toBe(expectedData);
-  // });
 });
---
+++ tests/utils.test.ts
@@ -0,0 +1,40 @@
+import {
+  parseParameterizedEndpointPath,
+  parseValueOrValuesAsArray,
+} from "../src/utils";
+
+describe("transforming a single value or an array of values into an array", () => {
+  it("should return an array with a single element of type T when given a primitive of type T", () => {
+    const elementTypes = ["a", 1, {}, null, undefined, NaN];
+
+    elementTypes.forEach((elementType) => {
+      const parsedArray = parseValueOrValuesAsArray(elementType);
+      expect(parsedArray).toHaveLength(1);
+      expect(parsedArray[0]).toBe(elementType);
+    });
+  });
+
+  it("should return the same array when given one", () => {
+    expect(parseValueOrValuesAsArray([1, 2])).toEqual([1, 2]);
+  });
+});
+
+describe("parsing an endpoint with path parameters", () => {
+  it.skip("should throw when missing a path parameter's value", () => {
+    expect(() =>
+      parseParameterizedEndpointPath("/{gameId}/{participantId}", {
+        gameId: 1,
+        something: 2,
+      })
+    ).toThrow();
+  });
+
+  it("should return a valid endpoint when given all path parameters", () => {
+    expect(
+      parseParameterizedEndpointPath("/{gameId}/{participantId}", {
+        gameId: "a",
+        participantId: 1,
+      })
+    ).toBe("/a/1");
+  });
+});
