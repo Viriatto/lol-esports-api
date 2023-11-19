7815067b6fff5ee92edbe13c1cfd48612d44bcfe
João Teixeira <jpedroteix99@gmail.com>
2023-11-19 22:12:30 +0000

    fix test

--- tests/interfaces/games.test.ts
+++ tests/interfaces/games.test.ts
@@ -11,10 +11,16 @@ describe("games' data fetching", () => {
   });
 
   describe("should return a valid response when fetching game window data", () => {
+    it("should return a valid response when not given a date", async () => {
+      const response = await gamesAPI.getWindow("110853167109116579");
+
+      expect(response.frames.length).toBeGreaterThanOrEqual(1);
+    });
+
     it("should return a valid response when given a date of type string", async () => {
       const response = await gamesAPI.getWindow(
-        "110853167109116577",
-        "2023-10-19T09:35:40.000Z"
+        "110853167109116579",
+        "2023-10-20T13:23:00.000Z"
       );
 
       expect(response.frames.length).toBeGreaterThanOrEqual(1);
